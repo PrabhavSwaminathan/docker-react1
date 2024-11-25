32261a9255f39e1c4cf6152edc7b004a2492715e
Prabhav Swaminathan <Prabhav@Tinas-MacBook-Pro.local>
2024-11-25 19:13:23 +1100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Nevin, stop staring at the wall</h1>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactx
+          Go and Learn React you lazy f**k
         </a>
       </header>
     </div>
